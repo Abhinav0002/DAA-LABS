4bb882e594c2951e8a23a631fa1c37601db5e076
Abhinav Mohan Bhatia <abhinavbhatia2@gmail.com>
2021-09-23 19:19:27 +0530

    DAA LABS

---
+++ WEEK 4/kthelement.java
@@ -0,0 +1,55 @@
+/* Abhinav Mohan Bhatia B.tech CSE SE 2015044
+    Given an unsorted array of integers, design an algorithm and implement it using a program to find Kth smallest
+    or largest element in the array. (Worst case Time Complexity = O(n)).
+Input Format:
+The first line contains number of test cases, T
+For each test case, there will be three input lines.
+First line contains n (the size of array). Second line contains space-separated integers describing array. 
+Third line contains K.
+Output Format:
+The output will have T number of lines.
+For each test case, output will be the Kth smallest or largest array element. 
+If no Kih element is present, output should be "not present".
+Input:-
+1
+5 
+7 10 4  3 15
+Output :-
+7
+ */
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+public class kthelement 
+{
+    // Function to return k'th smallest
+    // element in a given array
+    public static int kthSmallest(int[] arr,int k)
+    {
+        // Sort the given array
+        Arrays.sort(arr);
+ 
+        // Return k'th element in
+        // the sorted array
+        return arr[k - 1];
+    }
+ 
+    // driver program
+    public static void main(String[] args)
+    {
+    	Scanner sc = new Scanner(System.in);
+    	int t = sc.nextInt();
+    	while(t-->0)
+    	{
+    	  int n = sc.nextInt();
+  		  int arr[] = new int[n];
+  		  for(int i=0 ; i < n ; i++)
+  		  {
+  			  arr[i] = sc.nextInt();
+  		  }
+    	
+          int k = sc.nextInt();
+          System.out.print("K'th smallest element is " + kthSmallest(arr, k));
+        }
+    }
+}
---
+++ WEEK 4/mergesort.java
@@ -0,0 +1,137 @@
+/* Abhinav Mohan Bhatia B.tech CSE SE 2015044
+   
+   Question: Given an unsored array of integers, design an algorithm and implement it unsing a program to sort an array of elements by dividing the array into two subarrays and combining these subarraysaber sorting each one of them.
+	Your program should also find number of comparisons and inversions during sorting the array.
+		Input Format:
+			The first line contains number of test cases, T.
+			For each test case, there will be two input lines.
+			Second line contains space-separated integers describing array
+			First line contains n (the size of array)
+		Output Format:	
+			The output will have T number of lines
+			For each test case T, there will be three output lines.
+			First line will give the sorted array
+			Second line will give total number of comparisons
+			Third line will give total number of inversions required.
+		Sample Input:
+			1
+			8
+			23 65 21 76 46 89 45 32
+		Sample Output:
+			21 23 32 45 46 65 76 89
+			comparisons = 16
+			inversions =
+   
+ */
+
+import java.util.Scanner;
+
+public class mergesort 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-- >0)
+		{
+		  int n = sc.nextInt();
+		  int arr[] = new int[n];
+		  for(int i=0 ; i < n ; i++)
+		  {
+			  arr[i] = sc.nextInt();
+		  }
+		  mergesort ob = new mergesort();
+		  int b=ob.sort(arr , 0 , arr.length-1);
+		  System.out.println("\nSorted array");
+	        for (int i = 0; i < n; ++i)
+	            System.out.print(arr[i] + " ");
+	        System.out.println();
+			System.out.println("Comparisons="+b);
+		}
+	        sc.close();
+		
+
+	}
+	public static int merge(int arr[] , int l , int m , int r)
+	{
+		// Merges two sub-arrays of arr[].
+	    // First sub-array is arr[l..m]
+	    // Second sub-array is arr[m+1..r]
+        // Find sizes of two subarrays to be merged
+	        int n1 = m - l + 1;
+	        int n2 = r - m;
+	  
+	        /* Create temp arrays */
+	        int L[] = new int[n1];
+	        int R[] = new int[n2];
+	  
+	        //Copy data to temp arrays
+	        for (int i = 0; i < n1; ++i)
+	            L[i] = arr[l + i];
+	        for (int j = 0; j < n2; ++j)
+	            R[j] = arr[m + 1 + j];
+	  
+	        /* Merge the temp arrays */
+	  
+	        // Initial indexes of first and second subarrays
+	        int i = 0, j = 0;
+	  
+	        // Initial index of merged subarray array
+	        int k = l;
+		int count =0;
+	        while (i < n1 && j < n2) 
+	        {
+	            if (L[i] <= R[j]) 
+	            {
+	                arr[k] = L[i];
+	                i++;
+			    count++;
+	            }
+	            else
+	            {
+	                arr[k] = R[j];
+	                j++;
+			    count++;
+	            }
+	            k++;
+	        }
+	  
+	        //Copy remaining elements of L[] if any 
+	        while (i < n1)
+	        {
+	            arr[k] = L[i];
+	            i++;
+	            k++;
+	        }
+	  
+	        // Copy remaining elements of R[] if any 
+	        while (j < n2)
+	        {
+	            arr[k] = R[j];
+	            j++;
+	            k++;
+	        }
+		return count;
+	    }
+	  
+	    // Main function that sorts arr[l..r] using merge()
+	   int sort(int arr[], int l, int r)
+	    {
+	        int a=0;
+		   if (l < r) 
+	        {
+	            // Find the middle point
+	            int m =l+ (r-l)/2;
+	  
+	            // Sort first and second halves
+	            sort(arr, l, m);
+	            sort(arr, m + 1, r);
+	  
+	            // Merge the sorted halves
+	            a= merge(arr, l, m, r);
+	        }
+		   return a;
+	    }
+}
