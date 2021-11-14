d81a6406d933d49429efedc1e0eb063188ffdbb2
Abhinav Mohan Bhatia <abhinavbhatia2@gmail.com>
2021-11-14 12:28:05 +0530

    Week 6

---
+++ WEEK 6/isPathDFS.java
@@ -0,0 +1,42 @@
+import java.util.*;
+class isPathDFS
+{
+    static boolean isPath(int arr[][])
+    {
+        arr[0][0] = 1;
+ 
+        for (int i = 1; i < 5; i++)
+            if (arr[0][i] != -1)
+                arr[0][i] = arr[0][i - 1];
+        for (int j = 1; j < 5; j++)
+            if (arr[j][0] != -1)
+                arr[j][0] = arr[j - 1][0];
+ 
+        for (int i = 1; i < 5; i++)
+            for (int j = 1; j < 5; j++)
+                if (arr[i][j] != -1)
+                    arr[i][j] = Math.max(arr[i][j - 1],
+                                        arr[i - 1][j]);
+ 
+        return (arr[5 - 1][5 - 1] == 1);
+    }
+      
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n;
+        n=sc.nextInt();
+        int arr[][]=new int[n][n];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        if (isPath(arr))
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+    }
+}
